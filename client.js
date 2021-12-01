d765996ddb1294fb1f592c4ffa10cf797adbe088
Mark Chuang <markchuangth@gmail.com>
2021-12-01 14:27:57 -0500

    removed connect function from play and put it into client

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243" ,
+    port: 50541
+  });
+  //Allows us to receive messages from the client
+    conn.on('data', (data) => {
+      console.log('Server says: ', data);
+    });
+
+  conn.on('connect', () => {
+    conn.write('Hello from client!');
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = connect

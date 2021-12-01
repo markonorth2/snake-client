19a6bb55c9b55ec0cbc5ced8e74527d0a275aa36
Mark Chuang <markchuangth@gmail.com>
2021-12-01 16:30:25 -0500

    Allow Ctrl + C to quit the process

--- play.js
+++ play.js
@@ -1,8 +1,16 @@
 const connect = require('./client')
 const net = require("net");
+const setupInput = require('./input')
 
 // establishes a connection with the game server
 
 
 console.log("Connecting ...");
 connect();
+
+
+// setup interface to handle user input from stdin
+
+
+
+setupInput();
